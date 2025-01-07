8dcf172e47ab5af28f2cf2c2c54ec9af2ef5d10a
Sumit chouhan <sumitkhan.dhana@gmail.com>
2025-01-07 16:36:33 +0530

    change hero image slider

--- public/assets/js/all-slider.js
+++ public/assets/js/all-slider.js
@@ -22,16 +22,16 @@ $(document).ready(function () {
 });
 $(document).ready(function () {
   $(".product_slider").slick({
-    dots: false, 
-    arrows: true, 
-    prevArrow: $(".custom_prev_arrow"), 
-    nextArrow: $(".custom_next_arrow"), 
-    infinite: true, 
-    speed: 300, 
+    dots: false,
+    arrows: true,
+    prevArrow: $(".custom_prev_arrow"),
+    nextArrow: $(".custom_next_arrow"),
+    infinite: true,
+    speed: 300,
     slidesToShow: 4,
-    slidesToScroll: 1, 
-    autoplay: true, 
-    autoplaySpeed: 2000, 
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
     responsive: [
       {
         breakpoint: 992,
@@ -55,4 +55,19 @@ $(document).ready(function () {
   });
 });
 
+$(document).ready(function () {
+  $(".hero_slider").slick({
+    dots: false,
+    arrows: true,
+    fade: true,
+    prevArrow: $(".hero_prev_arrow"),
+    nextArrow: $(".hero_next_arrow"),
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
 
